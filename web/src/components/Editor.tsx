4a373bda677db01a4f316162aa9dc775052306b4
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2026-02-10 19:56:12 +0300

    Magic and Undo buttons

--- web/src/components/Editor.tsx
+++ web/src/components/Editor.tsx
@@ -1,15 +1,19 @@
-import { useEffect, useMemo, useState } from 'react';
-import { history } from '@codemirror/commands';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { history, undo } from '@codemirror/commands';
 import { StreamLanguage } from '@codemirror/language';
 import { lua } from '@codemirror/legacy-modes/mode/lua';
 import { Compartment } from '@codemirror/state';
 import { keymap, type EditorView } from '@codemirror/view';
-import { Box, useTheme } from '@mui/material';
+import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh';
+import UndoIcon from '@mui/icons-material/Undo';
+import { Box, IconButton, Tooltip, useTheme } from '@mui/material';
 import { type SxProps } from '@mui/system/styleFunctionSx';
 import { vscodeDark, vscodeLight } from '@uiw/codemirror-theme-vscode';
 import ReactCodeMirror from '@uiw/react-codemirror';
 
 import type { FilenamesRequest } from '@/api/schema';
+
+import { useTranslation } from '@/hooks/useTranslation';
 
 import { codeMirrorLangLog } from '@/utils/codeMirrorLangLog';
 import {
@@ -42,7 +46,10 @@ export const Editor = ({
 }: EditorProps) => {
   const { palette } = useTheme();
 
+  const { t } = useTranslation();
+
   const [editorView, setEditorView] = useState<EditorView | null>(null);
+  const [changed, setChanged] = useState(false);
 
   const extensions = useMemo(() => {
     const result = [historyCompartment.of([])];
@@ -73,6 +80,34 @@ export const Editor = ({
 
     return result;
   }, [type, onSave]);
+
+  const handleMagicButton = useCallback(() => {
+    const text = editorView?.state.doc.toString().trim();
+    if (!editorView || !text) {
+      return;
+    }
+
+    const lines = text.split('\n');
+    const data = Array.from(new Set(lines))
+      .map((line) => line.trim())
+      .filter((line) => line.length > 0)
+      .sort()
+      .join('\n');
+
+    editorView.dispatch({
+      changes: {
+        from: 0,
+        to: editorView.state.doc.length,
+        insert: data,
+      },
+    });
+  }, [editorView]);
+
+  const handleUndoButton = useCallback(() => {
+    if (editorView) {
+      undo({ state: editorView.state, dispatch: editorView.dispatch });
+    }
+  }, [editorView]);
 
   useEffect(() => {
     editorView?.dispatch({
@@ -121,6 +156,7 @@ export const Editor = ({
         editable={!readonly}
         onCreateEditor={(view) => setEditorView(view)}
         onChange={(newValue) => {
+          setChanged(value !== newValue);
           onChange?.(newValue, value !== newValue);
         }}
         basicSetup={{
@@ -131,6 +167,64 @@ export const Editor = ({
         }}
         extensions={extensions}
       />
+
+      {type !== 'arg' && !readonly && (
+        <Tooltip
+          title={t('editor.undo_button')}
+          enterTouchDelay={0}
+          placement="left"
+        >
+          <IconButton
+            size="small"
+            onClick={handleUndoButton}
+            disabled={!changed}
+            sx={{
+              position: 'absolute',
+              right: '6pt',
+              top: '6pt',
+              opacity: 0.5,
+              color: 'text.secondary',
+              transition: 'color 0.1s ease-in-out, opacity 0.1s ease-in-out',
+              minWidth: 0,
+              '&:hover': {
+                opacity: 1,
+                color: 'primary.main',
+              },
+            }}
+          >
+            <UndoIcon fontSize="small" />
+          </IconButton>
+        </Tooltip>
+      )}
+
+      {type === 'list' && !readonly && (
+        <Tooltip
+          title={t('editor.magic_button')}
+          enterTouchDelay={0}
+          placement="left"
+        >
+          <IconButton
+            size="small"
+            onClick={handleMagicButton}
+            sx={{
+              position: 'absolute',
+              right: '6pt',
+              top: '6pt',
+              transform: 'translateY(120%)',
+              opacity: 0.5,
+              color: 'text.secondary',
+              transition: 'color 0.1s ease-in-out, opacity 0.1s ease-in-out',
+              minWidth: 0,
+              '&:hover': {
+                opacity: 1,
+                color: 'primary.main',
+              },
+            }}
+          >
+            <AutoFixHighIcon fontSize="small" />
+          </IconButton>
+        </Tooltip>
+      )}
     </Box>
   );
 };
